25807976707792498e96a056bfaf146e37c56436
breeze256 <breeze256@foxmail.com>
2025-01-18 13:47:27 +0800

    First initial.

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "The flake for LUMO site.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.hugo
+          ];
+        };
+      });
+}
