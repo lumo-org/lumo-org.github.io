81db6670a3f99490e2c51999a4abef9182334578
breeze256 <breeze256@foxmail.com>
2025-01-20 07:31:04 +0000

    Add toggle support

---
+++ assets/javascripts/navbar-collapse.js
@@ -0,0 +1,5 @@
+var navbarToggle = document.getElementById("navbarToggle");
+var navbar = document.getElementById("navbar");
+navbarToggle.addEventListener("click", function() {
+    navbar.classList.toggle("hidden");
+});
